725b45d32831087ccbb6ca3c1877c8c53dbe9bf3
Craig J Perry <craigp84@gmail.com>
2023-03-01 20:41:24 +0000

    fix code copy script

--- _enabled-commands/copycode.sh
+++ _enabled-commands/copycode.sh
@@ -16,20 +16,17 @@
 
 USERNAME=$(id -u -n)
 
+# Need to give raycast whole disk access
 result=$(sqlite3 /Users/$USERNAME/Library/Messages/chat.db 'SELECT text FROM message WHERE datetime(date/1000000000 + 978307200,"unixepoch","localtime") > datetime("now","localtime","-60 second") ORDER BY date DESC LIMIT 1;')
 
-#You can append another `keyword` into the list to support other language Messages.
-keyword=("验证码" "code");
-keyword_regex="^(.*)(${keyword[*]/%/|})(.*)$"
 
-if [ ! $result ]; then
+if [[ -z "$result" ]]; then
   echo "No verification code received in the last 60 seconds!"
   exit 0;
 fi
 
-if [[ "$result" =~ "$keyword_regex" ]]; then
-  code=`echo $result | grep -o "[0-9]\{4,6\}"`;
-
+code=`echo $result | grep -o "[0-9]\{4,6\}"`;
+if [[ -n "$code" ]]; then
   echo "$code" | pbcopy;
   echo "$code code copied!"
 fi
